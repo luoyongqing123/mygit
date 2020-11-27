ccbb9a902a6621a2a39009a43756cbbe2753f92f
user <1649043741@qq.com>
2020-11-27 14:59:56 +0800

    C1客户端操作

--- src/main/java/com/git/Test02.java
+++ src/main/java/com/git/Test02.java
@@ -3,5 +3,6 @@ package com.git;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("Test02.......");
+        System.out.println("C1客户端操作-->Test02.......");
     }
 }
