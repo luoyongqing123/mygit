5f44d7e135d57fc60c8dd130fd41a14eb1b63de5
user <1649043741@qq.com>
2020-11-27 14:02:33 +0800

    修改Test01.java

--- src/main/java/com/git/Test01.java
+++ src/main/java/com/git/Test01.java
@@ -3,5 +3,8 @@ package com.git;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01.....");
+        System.out.println("test02.....");
+        System.out.println("test03.....");
+        System.out.println("test03.....");
     }
 }
